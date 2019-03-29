58fd15d877b7b9f618d9d8d63539cd8bd9088ae9
Aaron Langley <aaronlangley@Admins-MacBook-Pro-82.local>
2019-03-29 14:25:44 -0700

    added pdf417 parser and updated README

--- src/parsers.js
+++ src/parsers.js
@@ -32,17 +32,57 @@ const parse_id = (str) => {
     return {
         first_name: name[1],
         last_name: name[0],
+        license_number: data[3],
         under_21: !!(age < 21),
         under_18: !!(age < 18),
         expired: !!(expired > 0),
         age: age,
-        dob: data[2],
-        doe: data[1]
+        date_of_birth: data[2],
+        expiration_date: data[1]
     };
 };
 
 /**
+ * Parses data from PDF417 barcode found on most state-issued identification cards
+ * @param {string} str
+ * @return {object|null}
+ */
+const parse_pdf417 = (str) => {
+    if(typeof str !== 'string') return null;
+    let main_exp = /(?<=ansi\s+\d{12}dl\w+dl)[\w\s]+(?=dcf)/gi;
+    let second_exp = /(?=dbb)|(?=dbl)|(?=dba)|(?=dac)|(?=dct)|(?=dbp)|(?=dcs)|(?=dbo)|(?=daa)|(?=daq)|(?=ddf)|(?=dde)|(?=ddg)|(?=dce)|(?=dad)|(?=dca)|(?=dcb)|(?=dbd)|(?=dbc)|(?=dau)|(?=dag)|(?=dai)|(?=daj)|(?=dak)/gi;
+    let _data = str.match(main_exp);
+    if(!_data) return null;
+    let data = _data[0].split(second_exp);
+    if(data && data.length >= 4) {
+        // GET INFO
+        let obj = loop_pdf417_data(data);
+        // FORMAT DATES
+        let dates = format_pdf417_dates(obj);
+        // GET AGE AND EXPIRATION STATUS
+        let now = new Date();
+        let age = date_diff_years(dates.date_of_birth, now);
+        let expired = date_diff_days(dates.expiration_date, now);
+        // FORMAT RETURN OBJECT
+        return {
+            first_name: obj.first_name,
+            last_name: obj.last_name,
+            license_number: obj.license_number,
+            under_21: !!(age < 21),
+            under_18: !!(age < 18),
+            expired: !!(expired > 0),
+            age: age,
+            date_of_birth: obj.date_of_birth,
+            expiration_date: obj.expiration_date
+        }
+    } else {
+        return null;
+    }
+};
+
+/**
  * Creates commandline prompt which parses input from keyboard and logs parsed object to console (for debugging/testing purposes)
+ * This function will loop test the input string to determine the source of data (credit card, pdf417 etc.) and will output the result
  */
 const cli = () => {
     const rl = read_line.createInterface({
@@ -53,14 +93,14 @@ const cli = () => {
         let stdout;
         if(/%B\d{10,20}\^\w+/.test(input)) {
             stdout = parse_card(input);
-        } else if(/%[a-z]+\^\w+/i.test(input)) {
+        } else if(/%[a-z\s]+\^\w+/i.test(input)) {
             stdout = parse_id(input);
+        } else if(/(?<=ansi\s+\d{12}dl\w+dl)[\w\s]+(?=dcf)/gi.test(input)) {
+            stdout = parse_pdf417(input);
         }
-        console.table(stdout);
+        console.log(stdout);
     });
 };
-
-// todo: parse_pdf417 function for barcode
 
 /**
  * Parses input from ID card
@@ -70,7 +110,9 @@ const cli = () => {
 const parse_string = (str) => {
     let exp = /(?=[^])(\w+[$]\w+)|(\w+[$]\w+)(?<=[^])|(?<=;\d{1,20}=\d{4})\d{8}(?=[?])|(?<=;\d{1,20}=)\d{4}(?=\d{8}[?])/gi;
     let _data = str.match(exp);
-    if(_data && _data.length >= 3) {
+    let license_number = str.match(/((?<=;\d{7})\d{6,10})/gi);
+    if(_data && license_number) _data.push(license_number[0]);
+    if(_data && _data.length >= 4) {
         return _data.map( str => {
             return str.toLowerCase();
         });
@@ -79,9 +121,9 @@ const parse_string = (str) => {
 };
 
 /**
- * Formats dates and returns age and expiration status from ID
+ * Formats dates and returns age and days from expiration from ID
  * @param {array<string>} data
- * @return {{age: number, expired: boolean}}
+ * @return {{age: number, expired: number}}
  */
 const format_dates = (data) => {
     // format dates
@@ -148,17 +190,63 @@ const map_card_data = (matches) => {
 
 /**
  * Gets the expiration date/status from credit card data
- * @param {array} data
+ * @param {string} str
  * @return {object}
  */
-const check_card_expiration = (data) => {
+const check_card_expiration = (str) => {
     let obj = {};
-    obj.expiration_date = data.trim().match(/\d{2}/g).reverse().join('/');
+    obj.expiration_date = str.trim().match(/\d{2}/g).reverse().join('/');
     let now = new Date();
     let expiration_date = new Date(+`20${obj.expiration_date.substr(3, 2)}`, +obj.expiration_date.substr(0, 2) - 1);
     let expired = date_diff_days(expiration_date, now);
     obj.expired = !!(expired > 0);
     return obj;
+};
+
+/**
+ * Loops through matches from regular expression to get ID information
+ * @param {array} data Array of matches of regular expression
+ * @return {{first_name: string, last_name: string, license_number: string, date_of_birth: string, expiration_date: string}}
+ */
+const loop_pdf417_data = (data) => {
+    let first_name, last_name, license_number, date_of_birth, expiration_date;
+    for (let str of data) {
+        if (/daa/gi.test(str)) {
+            let arr = str.split(/\W/);
+            first_name = `${arr[1]} ${arr[2]}`.toLowerCase();
+            last_name = arr[0].match(/(?<=daa).*/i)[0].toLowerCase();
+        } else if (/dac|dct|dbp/gi.test(str)) {
+            first_name = str.match(/(?<=dac|dct|dbp)(.*)/gi)[0].toLowerCase().trim();
+        } else if (/dcs|dbo/gi.test(str)) {
+            last_name = str.match(/(?<=dcs|dbo)(.*)/gi)[0].toLowerCase().trim();
+        } else if (/daq/gi.test(str)) {
+            license_number = str.match(/(?<=daq)(\w+)/gi)[0];
+        } else if(/dbb|dbl/gi.test(str)) {
+            date_of_birth = str.match(/(?<=dbb|dbl)[0-9]{8}/gi)[0];
+        } else if(/dba/gi.test(str)) {
+            expiration_date = str.match(/(?<=dba)[0-9]{8}/gi)[0];
+        }
+    }
+    return {first_name, last_name, license_number, date_of_birth, expiration_date};
+};
+
+/**
+ * Formats birth date and expiration date into JavaScript Date objects
+ * @param {object} obj
+ * @param {string} obj.date_of_birth Date of birth string as parsed from barcode
+ * @param {string} obj.expiration_date Expiration date string as parsed from barcode
+ * @return {{date_of_birth: Date, expiration_date: Date}}
+ */
+const format_pdf417_dates = (obj) => {
+    let expiration_date, date_of_birth;
+    if(parseInt(obj.date_of_birth.match(/\d{4}/)[0]) < 1920 && parseInt(obj.expiration_date.match(/\d{4}/)[0]) < 1900) {
+        date_of_birth = new Date(+obj.date_of_birth.substr(4,4), +obj.date_of_birth.substr(2,2), +obj.date_of_birth.substr(0,2));
+        expiration_date = new Date(+obj.expiration_date.substr(4,4), +obj.expiration_date.substr(2,2), +obj.expiration_date.substr(0,2))
+    } else {
+        date_of_birth = new Date(+obj.date_of_birth.substr(0,4), +obj.date_of_birth.substr(4,2), +obj.date_of_birth.substr(6,2));
+        expiration_date = new Date(+obj.expiration_date.substr(0,4), +obj.expiration_date.substr(4,2), +obj.expiration_date.substr(6,2));
+    }
+    return {date_of_birth, expiration_date};
 };
 
 module.exports = {
@@ -167,5 +255,8 @@ module.exports = {
     date_diff_years,
     parse_id,
     parse_card,
+    parse_pdf417,
+    format_pdf417_dates,
+    loop_pdf417_data,
     cli
 };
--- tests/parsers.test.js
+++ tests/parsers.test.js
@@ -4,10 +4,14 @@ const credit_str = samples.credit_card;
 const credit_str_exp = samples.credit_card_expired;
 const id_str = samples.id;
 const id_str_exp = samples.id_expired;
-const bad_param_arr = ['%E?;E?', 'asdfasdfasdf12345', '', '%CAHUNTINGTN BC^DOE$JOHN$MIDDLE', 1234567, true, false, '%B1234567812345678^DOE/JOHN ^2311201107631100000000580000'];
+const pdf417_id = samples.pdf417_id;
+const pdf417_id_expired = samples.pdf417_id_expired;
+const bad_param_arr = ['%E?;E?', 'asdfasdfasdf12345', '', '%CAHUNTINGTN BC^DOE$JOHN$MIDDLE', 1234567, true, false, '%B1234567812345678^DOE/JOHN ^2311201107631100000000580000', pdf417_id.substr(0,28)];
 
+// INTEGRATION TESTS
 test('should parse data from credit card mag stripe', () => {
     expect(parser.parse_card(id_str)).toBeNull();
+    expect(parser.parse_card(pdf417_id)).toBeNull();
     expect(parser.parse_card(credit_str)).toEqual(
         {
             card_number: 1234567812345678,
@@ -28,30 +32,63 @@ test('should parse data from credit card mag stripe', () => {
 
 test('should parse data from id card mag stripe', () => {
     expect(parser.parse_id(credit_str)).toBeNull();
+    expect(parser.parse_id(pdf417_id)).toBeNull();
     expect(parser.parse_id(id_str)).toEqual(
         {
             first_name: 'john',
             last_name: 'doe',
+            license_number: '81234567',
             under_21: false,
             under_18: false,
             expired: false,
             age: 25,
-            dob: '19931223',
-            doe: '2112'
+            date_of_birth: '19931223',
+            expiration_date: '2112'
         });
     expect(parser.parse_id(id_str_exp)).toEqual(
         {
             first_name: 'john',
             last_name: 'doe',
+            license_number: '81234567',
             under_21: false,
             under_18: false,
             expired: true,
             age: 25,
-            dob: '19931223',
-            doe: '1812'
+            date_of_birth: '19931223',
+            expiration_date: '1812'
         });
 });
 
+test('should parse data from PDF417 barcode found on most state-issued ID cards', () => {
+    expect(parser.parse_pdf417(credit_str)).toBeNull();
+    expect(parser.parse_pdf417(id_str)).toBeNull();
+    expect(parser.parse_pdf417(pdf417_id)).toEqual(
+        {
+            first_name: 'jane',
+            last_name: 'doe',
+            license_number: '123456789',
+            under_21: false,
+            under_18: false,
+            expired: false,
+            age: 24,
+            date_of_birth: '05081994',
+            expiration_date: '05082022'
+        });
+    expect(parser.parse_pdf417(pdf417_id_expired)).toEqual(
+        {
+            first_name: 'jane',
+            last_name: 'doe',
+            license_number: '123456789',
+            under_21: false,
+            under_18: false,
+            expired: true,
+            age: 24,
+            date_of_birth: '05081994',
+            expiration_date: '05082018'
+        });
+});
+
+// UNIT TESTS
 test('should return a delta of days', () => {
     let now = new Date(2019, 1, 1), then = new Date(2018, 1, 1);
     expect(parser.date_diff_days(then, now)).toBe(365);
@@ -64,18 +101,38 @@ test('should return a delta of years', () => {
 
 test('parse string input from card reader', () => {
     expect(parser.parse_string('asdfasdfasdf')).toBeNull();
-    expect(parser.parse_string(id_str)).toEqual([ 'doe$john', '2112', '19931223' ]);
+    expect(parser.parse_string(id_str)).toEqual([ 'doe$john', '2112', '19931223', '81234567' ]);
+});
+
+test('should loop through array of matches and return a formatted object', () => {
+    expect(parser.loop_pdf417_data(['asdfasdf', 'asdfasdf', 'asdfasdf']));
+    expect(parser.loop_pdf417_data(samples.pdf_417_array)).toEqual(
+        samples.pdf_417_obj
+    )
+});
+
+test('should convert parsed dates to JavaScript date objects', () => {
+    expect(parser.format_pdf417_dates(samples.pdf_417_obj)).toEqual({
+            date_of_birth: new Date('1994-09-05T07:00:00.000Z'),
+            expiration_date: new Date('2022-09-05T07:00:00.000Z')
+        });
 });
 
 // FAILED TESTS
-test('parse_id should fail to parse bad input', () => {
+test('parse_id should fail to parse bad/incomplete input', () => {
     bad_param_arr.forEach(str => {
         expect(parser.parse_id(str)).toBeNull();
     });
 });
 
-test('parse_card should fail to parse bad input', () => {
+test('parse_card should fail to parse bad/incomplete input', () => {
     bad_param_arr.forEach(str => {
         expect(parser.parse_card(str)).toBeNull();
+    });
+});
+
+test('parse_pdf417_id should fail to parse bad/incomplete input', () => {
+    bad_param_arr.forEach(str => {
+        expect(parser.parse_pdf417(str)).toBeNull();
     });
 });
